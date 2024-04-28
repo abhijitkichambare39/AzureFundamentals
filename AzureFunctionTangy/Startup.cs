b1d92c20f2638fadcde11fc154fb277ccb618a1f
abhijitkichambare39 <76390929+abhijitkichambare39@users.noreply.github.com>
2024-04-28 16:48:12 +0530

    Azure Functions Vs 2022 (#5)
    * initial commit
    
    * template project creation
    
    * azure function SQL DB

---
+++ AzureFunctionTangy/Startup.cs
@@ -0,0 +1,30 @@
+﻿using AzureFunctionTangy;
+using AzureFunctionTangy.Data;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+[assembly: WebJobsStartup(typeof(Startup))]
+namespace AzureFunctionTangy
+{
+    public class Startup : IWebJobsStartup
+    {
+        public void Configure(IWebJobsBuilder builder)
+        {
+            string connectionString = Environment.GetEnvironmentVariable("AzureSQLDatabase");
+
+            builder.Services.AddDbContext<AzureTangyDbContext>(
+                options => options.UseSqlServer(connectionString)
+            );
+
+            builder.Services.BuildServiceProvider();
+        }
+    }
+}
--- AzureFunctionTangyWeb/Controllers/HomeController.cs
+++ AzureFunctionTangyWeb/Controllers/HomeController.cs
@@ -1,12 +1,15 @@
 ﻿using AzureFunctionTangyWeb.Models;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 using System.Diagnostics;
+using System.Text.Json.Serialization;
 
 namespace AzureFunctionTangyWeb.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        static readonly HttpClient _httpClient = new HttpClient();
 
         public HomeController(ILogger<HomeController> logger)
         {
@@ -18,6 +21,20 @@ namespace AzureFunctionTangyWeb.Controllers
             return View();
         }
 
+
+
+        [HttpPost]
+        public async Task<IActionResult> Index(SalesRequest salesRequest)
+        {
+            salesRequest.Id = Guid.NewGuid().ToString();
+            using (var content = new StringContent(JsonConvert.SerializeObject(salesRequest), System.Text.Encoding.UTF8, "application/json"))
+            {
+                //call out function and pass content
+                HttpResponseMessage responseMessage = await _httpClient.PostAsync(@"http://localhost:7152/api/OnSalesUploadingWriteToQueue", content);
+                string returnValue = responseMessage.Content.ReadAsStringAsync().Result;
+            }
+            return RedirectToAction(nameof(Index));
+        }
         public IActionResult Privacy()
         {
             return View();
