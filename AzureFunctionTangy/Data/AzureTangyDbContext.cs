550a0ecf199022caf9511ae1dd58d5321bece18a
abhijitkichambare39 <abhijitkichambare39@gmail.com>
2024-04-28 16:47:17 +0530

    azure function SQL DB

---
+++ AzureFunctionTangy/Data/AzureTangyDbContext.cs
@@ -0,0 +1,31 @@
+﻿using AzureFunctionTangy.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AzureFunctionTangy.Data
+{
+    public class AzureTangyDbContext : DbContext
+    {
+
+        public AzureTangyDbContext(DbContextOptions<AzureTangyDbContext> dbContextOptions) : base(dbContextOptions)
+        {
+
+        }
+
+        public DbSet<SalesRequest> SalesRequests { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<SalesRequest>(entity =>
+            {
+                entity.HasKey(c => c.Id);
+            });
+        }
+    }
+}
